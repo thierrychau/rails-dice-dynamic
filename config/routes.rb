60c6b178d92daca7cea40723de84266718dae806
Thierry Chau <thierry.chau@gmail.com>
2023-09-26 02:32:27 +0000

    deleted merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get("/dice/:first_die/:second_die", { :controller => "dice", :action => "results"})
-=======
   get("/", { :controller => "pages", :action => "show" })
 
   get("/dice/:number_of_dice/:how_many_sides", { :controller => "pages", :action => "dice" })
->>>>>>> df0da9f (initial commit)
 end
